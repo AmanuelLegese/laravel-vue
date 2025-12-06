9bb91bb4acbddf9230995ade986c2f6fa07ae80d
AmanuelLegese <Amleaz2005@gmail.com>
2025-12-06 20:24:39 +0300

    auth permission role item

---
+++ app/Http/Requests/AssignUserToRoleRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AssignUserToRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'roles' => 'required|array',
+            'roles.*' => 'required|integer|exists:roles,id',
+            'users' => 'required|array',
+            'users.*' => 'required|integer|exists:users,id',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,29 @@ use App\Http\Controllers\AuthController;
 
     Route::post('login', AuthController::class . '@login');
 
+    Route::post('logout', AuthController::class . '@logout');
 
     /**
      * protected routes
      */
     Route::middleware('auth:api')->group(function () {
         Route::apiResource('/items', App\Http\Controllers\API\ItemController::class);
+        /**
+         * Permission Management
+         */
+        Route::get('/permission', [App\Http\Controllers\API\ManagePermission::class, 'getPermission']);
+        Route::post('/grant', [App\Http\Controllers\API\ManagePermission::class, 'grantPermission']);
+        Route::post('/revoke', [App\Http\Controllers\API\ManagePermission::class, 'revokePermission']);
+        Route::post('/massgrant', [App\Http\Controllers\API\ManagePermission::class, 'massgrantPermission']);
+        Route::post('/massrevoke', [App\Http\Controllers\API\ManagePermission::class, 'massrevokePermission']);
+        /**
+         * Role Management
+         */
+        Route::get('/roles', App\Http\Controllers\API\ManageRole::class);
+        Route::post('/roles', [App\Http\Controllers\API\ManageRole::class, 'createRole']);
+        Route::delete('/roles', [App\Http\Controllers\API\ManageRole::class, 'deleteRole']);
+        Route::post('/assign-permissions-roles', [App\Http\Controllers\API\ManageRole::class, 'assignPermissionsToRole']);
+        Route::post('/revoke-permissions-roles', [App\Http\Controllers\API\ManageRole::class, 'revokePermissionsFromRole']);
+        Route::post('/assign-user-role', [App\Http\Controllers\API\ManageRole::class, 'assignUserToRole']);
+        Route::post('/revoke-user-role', [App\Http\Controllers\API\ManageRole::class, 'revokeUserFromRole']);
     });
